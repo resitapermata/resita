6ba746cc251f07a33211320e58f9bfd4d15bba5f
Resita Permata <resitapermatasacn@gmail.com>
2022-05-12 22:42:52 +0700

    ok

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="px-[375px] py-[67px] flex items-center bg-blue">
+      <div>
+        <div className="text-[12px] font-white">wknd@2020</div>
+        <h4 className="font-bold text-base">Fellas</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
