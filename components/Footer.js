79087efa80379276c5322d76618d5a658e4780a1
Resita Permata <resitapermatasacn@gmail.com>
2022-05-15 14:10:06 +0700

    baru 2

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" flex bg-[#0B24FB] text-white space-x-[160px] sm:space-x-[716px]">
+    <div className=" flex bg-[#0B24FB] text-white space-x-[160px]">
       <div className="px-[24px] py-[24px] text-[12px] sm:px-[213px]">
         <span className="font-bold">wknd@</span>2020
       </div>
